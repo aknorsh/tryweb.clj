11de6c736eaff522b7f33ad4985aaece90ec4f75
AKINORI Ishii <aknorsh@gmail.com>
2019-12-26 21:32:39 +0900

    Split files

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -1,21 +1,20 @@
 (ns todo-clj.core
-    (:require [compojure.core :refer [defroutes context GET]]
-              [compojure.route :as route]
-              [ring.adapter.jetty :as server]
-              [ring.util.response :as res]))
+    (:require [compojure.core :refer [routes]]
+     [ring.adapter.jetty :as server]
+     [todo-clj.handler.main :refer [main-routes]]
+     [todo-clj.handler.todo :refer [todo-routes]]))
 
-; SERVER
 
 (defonce server (atom nil))
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, World!"})
+(def app
+  (routes
+    todo-routes
+    main-routes))
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
@@ -28,56 +27,28 @@
     (start-server)))
 
 
-; ROUTING
-
-(defn html [res]
-  (res/content-type res "text/html; charset=utf-8"))
 
 
-(defn home-view [req]
-  "<h1>HOME</h1>
-   <a href=\"/todo\">TODO</a>")
 
-(defn home [req]
-  (-> (home-view req)
-      res/response
-      html))
 
-(def todo-list
-  [{:title "Take a train"}
-   {:title "Ride an elevator"}
-   {:title "Work"}
-   {:title "Go home"}])
 
-(defn todo-index-view [req]
-  `("<h1>TODO</h1>"
-    "<ul>"
-    ~@(for [{:keys [title]} todo-list]
-           (str "<li>" title "</li>"))
-    "</ul>"))
 
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      res/response
-      html))
 
-(def routes
-  {"/" home
-   "/todo" todo-index})
 
-(defn match-route [uri]
-  (get routes uri))
 
-(defn handler [req]
-  (let [uri (:uri req)
-        maybe-fn (match-route uri)]
-    (if maybe-fn
-        (maybe-fn req)
-        (not-found))))
 
-; with compojure
 
-(defroutes handler
-  (GET "/" req home)
-  (GET "/todo" req todo-index)
-  (route/not-found "<h1>404 page not found.</h1>"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
