0c5ff33d4684149a22754634adf30bbfa002827f
AKINORI Ishii <aknorsh@gmail.com>
2019-12-26 20:30:54 +0900

    Impl: routing

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -1,6 +1,8 @@
 (ns todo-clj.core
     (:require [ring.adapter.jetty :as server]))
 
+; SERVER
+
 (defonce server (atom nil))
 
 (defn handler [req]
@@ -21,3 +23,60 @@
   (when @server
     (stop-server)
     (start-server)))
+
+
+; ROUTING
+
+(defn ok [body]
+  {:status 200
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found<h1>"})
+
+
+(defn home-view [req]
+  "<h1>HOME</h1>
+   <a href=\"/todo\">TODO</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+
+(def todo-list
+  [{:title "Take a train"}
+   {:title "Ride an elevator"}
+   {:title "Work"}
+   {:title "Go home"}])
+
+(defn todo-index-view [req]
+  `("<h1>TODO</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} todo-list]
+           (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+(defn match-route [uri]
+  (get routes uri))
+
+(defn handler [req]
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+        (maybe-fn req)
+        (not-found))))
