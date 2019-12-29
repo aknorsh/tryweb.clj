8e507123ba35636cbe1216b5abfb27c8b064437c
AKINORI Ishii <aknorsh@gmail.com>
2019-12-29 22:11:20 +0900

    Enable: add todo

--- todo-clj/src/todo_clj/view/todo.clj
+++ todo-clj/src/todo_clj/view/todo.clj
@@ -1,5 +1,7 @@
 (ns todo-clj.view.todo
-    (:require [todo-clj.view.layout :as layout]))
+    (:require 
+      [hiccup.form :as hf]
+      [todo-clj.view.layout :as layout]))
 
 (defn todo-index-view [req todo-list]
   (->> `([:h1 "TODOs"]
@@ -8,4 +10,16 @@
                   [:li title])])
     (layout/common req)))
 
+(defn todo-new-view [req]
+  (->> [:section.card
+        [:h2 "TODO Add"]
+        (hf/form-to
+          [:post "/todo/new"]
+          [:input {:name :title :placeholder "Input TODO"}]
+          [:button.bg-blue "ADD"])]
+       (layout/common req)))
 
+(defn todo-complete-view [req]
+  (->> [:section.card
+        [:h2 "Complete TODO add."]]
+       (layout/common req)))
